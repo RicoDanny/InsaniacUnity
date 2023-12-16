44ee70e9e5e2f7e3c4dcc507529a73fd23e36821
InsaniacCrew <Incsaniac@gmail.com>
2023-12-16 23:04:36 +0100

    Finito

--- Assets/Code/OtherCode/ForceCameraAspectRatio.cs
+++ Assets/Code/OtherCode/ForceCameraAspectRatio.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ForceCameraAspectRatio : MonoBehaviour
 {
-    void Start () 
+    void Update () 
     {
         // set the desired aspect ratio (the values in this example are
         // hard-coded for 16:9, but you could make them into public
